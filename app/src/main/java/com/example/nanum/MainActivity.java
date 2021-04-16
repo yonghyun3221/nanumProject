6457f6058d50dd5aa7971359fecf9b61407e6715
yongh <yonghyun3221@gmail.com>
2021-04-17 06:01:15 +0900

    create

---
+++ app/src/main/java/com/example/nanum/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.nanum;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView)findViewById(R.id.imageView3);
+        image.setVisibility(View.VISIBLE);
+
+        image.setImageResource(R.drawable.pic);
+
+
+    }
+}
