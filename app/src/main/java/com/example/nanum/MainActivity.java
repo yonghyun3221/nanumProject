2191c0538d9aaca8859d4cbb15cfff17be304027
yongh <yonghyun3221@gmail.com>
2021-04-26 01:41:42 +0900

    Toast

--- app/src/main/java/com/example/nanum/MainActivity.java
+++ app/src/main/java/com/example/nanum/MainActivity.java
@@ -2,24 +2,43 @@ package com.example.nanum;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView image;
+    ImageView mainImage;
+    Button signInBtn, signUpBtn;
+    EditText emailText, pwText;
+
+
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image = (ImageView)findViewById(R.id.imageView3);
-        image.setVisibility(View.VISIBLE);
+        mainImage = (ImageView)findViewById(R.id.imageView3);
+        signInBtn = (Button)findViewById(R.id.SignInBtn);
+        signUpBtn = (Button)findViewById(R.id.SignUpBtn);
+        emailText = (EditText)findViewById(R.id.EmailTextField);
+        pwText = (EditText) findViewById(R.id.PWTextField);
 
-        image.setImageResource(R.drawable.pic);
+        mainImage.setVisibility(View.VISIBLE);
+        mainImage.setImageResource(R.drawable.pic);
 
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = emailText.getText().toString();
+                Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
