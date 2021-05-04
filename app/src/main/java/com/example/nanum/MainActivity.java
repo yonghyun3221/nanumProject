d0dd8f7e481f0944675e4bbf4fd5b1680bae7a83
yongh <yonghyun3221@gmail.com>
2021-05-04 20:22:55 +0900

    SignUp

--- app/src/main/java/com/example/nanum/MainActivity.java
+++ app/src/main/java/com/example/nanum/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nanum;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,9 +46,69 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 text = emailText.getText().toString();
-                Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
+
+
+                System.out.println("onclick");
+
+
+
+
+                new Thread(){
+
+                    @Override
+                    public void run() {
+
+                        System.out.println("runThread");
+
+                        try {
+                            System.out.println("run");
+
+                            URL url = new URL("http://116.39.48.151:3000/users");
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            connection.setRequestMethod("GET"); //전송방식
+                            connection.setDoOutput(false);       //데이터를 쓸 지 설정
+                            connection.setDoInput(true);        //데이터를 읽어올지 설정
+
+                            InputStream is = connection.getInputStream();
+                            StringBuilder sb = new StringBuilder();
+                            BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+                            String result;
+                            int count = 0;
+                            while((result = br.readLine())!=null){
+                                count++;
+                                System.out.println("br = ");
+                                System.out.println(count);
+
+                                sb.append(result+"\n");
+                            }
+
+                            System.out.println(sb);
+
+
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
             }
         });
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/nanum/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.nanum;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+
+
+
+
+    }
+}
