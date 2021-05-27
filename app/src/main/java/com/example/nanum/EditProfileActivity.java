f948bf04504b40da2e551c3d5ac748581c201774
engjuyeon <engjeong43@gmail.com>
2021-05-27 13:02:37 +0900

    Profile edit

---
+++ app/src/main/java/com/example/nanum/EditProfileActivity.java
@@ -0,0 +1,6 @@
+package com.example.nanum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/example/nanum/ProfileTabFragment.java
+++ app/src/main/java/com/example/nanum/ProfileTabFragment.java
@@ -1,10 +1,13 @@
 package com.example.nanum;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,14 +18,38 @@ public class ProfileTabFragment extends Fragment // Fragment 클래스를 상속
 
     private View view;
     TextView text;
+    Button editPro, logout;
+    // 마지막으로 뒤로 가기 버튼을 눌렀던 시간 저장
+    private long backKeyPressedTime = 0;
+    // 첫 번째 뒤로 가기 버튼을 누를 때 표시
+    private Toast toast;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.profile_layout,container,false);
-        text = view.findViewById(R.id.textView10);
+        editPro = (Button)view.findViewById(R.id.editProfile);
 
+
+        editPro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( view.getContext(), EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        logout = (Button)view.findViewById(R.id.logout);
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( view.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
+
 }
