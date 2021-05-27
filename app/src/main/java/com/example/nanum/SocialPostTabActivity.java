f537c32a463ae025f2dc23460acef469f645fb7f
engjuyeon <engjeong43@gmail.com>
2021-05-27 09:55:14 +0900

    SocialPostTabActivity

---
+++ app/src/main/java/com/example/nanum/SocialPostTabActivity.java
@@ -0,0 +1,18 @@
+package com.example.nanum;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStream;
+
+public class SocialPostTabActivity  extends AppCompatActivity {
+}
