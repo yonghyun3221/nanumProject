8ffc1f089020a4e334f5ec75f11c74c13c98e64a
yongh <yonghyun3221@gmail.com>
2021-05-29 05:52:05 +0900

    google login & out

--- app/src/main/java/com/example/nanum/LoginActivity.java
+++ app/src/main/java/com/example/nanum/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.nanum;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,15 +23,29 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    Boolean logout = true;
     TabLayout tabLayout;
     ViewPager viewPager;
-    FloatingActionButton google;
+    SignInButton google;
     Context context;
     EditText email, passwd;
     Button login;
     String text;
+
+    public GoogleSignInClient mGoogleSignInClient;
+    private final int RC_SIGN_IN = 123;
+    private static final String TAG = "MainActivity";
 
     float v=0;
     @Override
@@ -36,9 +54,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        google = findViewById(R.id.sign_in_button);
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.viewPager);
-        google = findViewById(R.id.fab_google);
         login = (Button)findViewById(R.id.login);
 
         tabLayout.addTab(tabLayout.newTab().setText("LOGIN"));
@@ -66,15 +84,103 @@ public class LoginActivity extends AppCompatActivity {
         email = (EditText)findViewById(R.id.email);
         passwd = (EditText) findViewById(R.id.passwd);
 
+        // 앱에 필요한 사용자 데이터를 요청하도록 로그인 옵션을 설정한다.
+        // DEFAULT_SIGN_IN parameter는 유저의 ID와 기본적인 프로필 정보를 요청하는데 사용된다.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail() // email addresses도 요청함
+                .build();
+
+        // 위에서 만든 GoogleSignInOptions을 사용해 GoogleSignInClient 객체를 만듬
+        mGoogleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+
+        // 기존에 로그인 했던 계정을 확인한다.
+        GoogleSignInAccount gsa = GoogleSignIn.getLastSignedInAccount(LoginActivity.this);
+
+        // 로그인 되있는 경우 (토큰으로 로그인 처리) 자동로그인
+        if (gsa != null && gsa.getId() != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(signIn())
+                {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
 
 
+            }
+        });
 
 
     }
+
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount acct = completedTask.getResult(ApiException.class);
+
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+
+                Log.d(TAG, "handleSignInResult:personName "+personName);
+                Log.d(TAG, "handleSignInResult:personGivenName "+personGivenName);
+                Log.d(TAG, "handleSignInResult:personEmail "+personEmail);
+                Log.d(TAG, "handleSignInResult:personId "+personId);
+                Log.d(TAG, "handleSignInResult:personFamilyName "+personFamilyName);
+                Log.d(TAG, "handleSignInResult:personPhoto "+personPhoto);
+            }
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.e(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+
+
+    private boolean signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
     @Override
     public void onBackPressed() {
         //super.onBackPressed();
     }
+
+    public GoogleSignInClient getGoogleSignInClient(){
+        return mGoogleSignInClient;
+    }
+
+    public void setlogout(boolean logout){
+        this.logout = logout;
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/nanum/ProfileTabFragment.java
+++ app/src/main/java/com/example/nanum/ProfileTabFragment.java
@@ -2,6 +2,7 @@ package com.example.nanum;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +14,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileTabFragment extends Fragment // Fragment 클래스를 상속받아야한다
 {
+    GoogleSignInClient mGoogleSignInClient;
+    private static final String TAG = "MainActivity";
 
     private View view;
     TextView text;
-    Button editPro, logout;
+    public Button editPro, logout;
     // 마지막으로 뒤로 가기 버튼을 눌렀던 시간 저장
     private long backKeyPressedTime = 0;
     // 첫 번째 뒤로 가기 버튼을 누를 때 표시
@@ -30,6 +41,14 @@ public class ProfileTabFragment extends Fragment // Fragment 클래스를 상속
     {
         view = inflater.inflate(R.layout.profile_layout,container,false);
         editPro = (Button)view.findViewById(R.id.editProfile);
+        logout = (Button)view.findViewById(R.id.logout);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail() // email addresses도 요청함
+                .build();
+
+        // 위에서 만든 GoogleSignInOptions을 사용해 GoogleSignInClient 객체를 만듬
+        mGoogleSignInClient = GoogleSignIn.getClient(inflater.getContext(), gso);
 
 
         editPro.setOnClickListener(new View.OnClickListener() {
@@ -39,16 +58,20 @@ public class ProfileTabFragment extends Fragment // Fragment 클래스를 상속
                 startActivity(intent);
             }
         });
-        logout = (Button)view.findViewById(R.id.logout);
+
 
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mGoogleSignInClient.revokeAccess();
+                mGoogleSignInClient.signOut();
+
                 Intent intent = new Intent( view.getContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
+
         return view;
     }
 
